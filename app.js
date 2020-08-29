c6f5d31db945d0d529ab6c436b91076fca7d15cd
SS Jones <SSJones.code@gmail.com>
2020-08-29 16:58:10 -0700

    I formatted the document in the intern, engineer, and manager files.js.

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
