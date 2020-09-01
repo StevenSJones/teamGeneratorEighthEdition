83e454ff9cc0dccb41fdfa39bab8f2cb16537da0
SS Jones <SSJones.code@gmail.com>
2020-09-01 09:39:44 -0700

    I added logic to the questions array of objects. I descructured the response to get the name off of it with the {} inside the engineer or intern .then callback function and I returned the second inquirer prompt so that it fires when the previous comes back asyncronously.

--- app.js
+++ app.js
@@ -14,126 +14,123 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// ### User input
-
-// The project must prompt the user to build an engineering team. An engineering
-// team consists of a manager, and any number of engineers and interns.
+const { listenerCount } = require("process");
 
 //empty array to house the emplyees
 var employeesArray = [];
-
-// array of questions for user to answer.
-const questions = [
-  {
-    type: "input",
-    message: "What is your Manager's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your Manager's id number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your Manager's email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your Manager's office number?",
-    name: "officeNumber",
-  },
-  {
-    type: "list",
-    message: "What type of team member would you like to add?",
-    choices: [
-      "Engineer",
-      "Intern",
-      "I don't want to add any more team members.",
-    ],
-    name: "role",
-  },
-  {
-    type: "input",
-    message: "What is your Engineer's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your Engineer's id number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your Engineer's email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your Engineer's GITHUB username?",
-    name: "github",
-  },
-  {
-    type: "list",
-    message: "What type of team member would you like to add?",
-    choices: [
-      "Engineer",
-      "Intern",
-      "I don't want to add any more team members.",
-    ],
-    name: "role",
-  },
-  {
-    type: "input",
-    message: "What is your Intern's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is your Itern's ID number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is your Intern's email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What school did your Intern attend?",
-    name: "school",
-  },
-  {
-    type: "list",
-    message: "What type of team member would you like to add?",
-    choices: [
-      "Engineer",
-      "Intern",
-      "I don't want to add any more team members.",
-    ],
-    name: "role",
-  },
-];
-//reference the var on line 1 and use the prompt method()
-//this is asyncronous
+// The project must prompt the user to build an engineering team. An engineering
+// team consists of a manager, and any number of engineers and interns.
 inquirer
-  .prompt(questions)
-  //below you see what happens WHEN the response returns
-  .then(function (response) {
-    console.log("response: ", response);
+  .prompt([
+    {
+      type: "input",
+      message: "What is your Manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your Manager's id number?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your Manager's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your Manager's office number?",
+      name: "officeNumber",
+    },
+  ])
+  //deals with the response from the manager questions
+  .then(function (manager) {
+    employeesArray.push(manager); //push manager to array
+    console.log("manager: ", manager);
+    console.log("employeesArray: ", employeesArray);
 
-    employeesArray.push(response);
-
-    console.log(employeesArray);
-
-    //const manager = new Manager (response);
-
-    //instead of stringify I am passing the response obj into the function and since the
+    return inquirer.prompt([
+      {
+        type: "list",
+        message: "who else would you like to add to the team?",
+        name: "engineerOrIntern",
+        choices: ["Engineer", "Intern", "Quit"],
+      },
+    ]);
+  })
+  // works to here
+  .then(function ({ engineerOrIntern }) {
+    //without curly brackets engineer or intern is an object
+    if (engineerOrIntern === "Engineer") {
+      // prompt engineer details
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is your Engineer's name?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's id number?",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's email address?",
+            name: "email",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's GITHUB username?",
+            name: "github",
+          },
+        ])
+        .then(function (engineer) {
+          console.log("engineer: ", engineer);
+          employeesArray.push(engineer); //push engineer to array
+          console.log(employeesArray);
+        });
+    } else if (engineerOrIntern === "Intern") {
+      //prompt for intern
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is your Intern's name?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What is your Itern's ID number?",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What is your Intern's email address?",
+            name: "email",
+          },
+          {
+            type: "input",
+            message: "What school did your Intern attend?",
+            name: "school",
+          },
+        ])
+        .then(function (intern) {
+          console.log("response: ", intern);
+          employeesArray.push(intern); //push intern to array
+          console.log(employeesArray);
+        });
+    } //intern
+    else {
+      //write to file
+      fs.writeFile("user.html", employeesArray, function (err) {
+        console.log("HTML file created!");
+      });
+    }
   });
+
+//const manager = new Manager (response);
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -144,10 +141,6 @@ inquirer
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
